533b8fbf33a0eeec16f6ae5ad9058cdafa4bea87
Japer Yu <007seadog@gmail.com>
2015-06-08 15:57:12 +0800

    Added another 4 sources

--- fetch_lots_site.sh
+++ fetch_lots_site.sh
@@ -5,8 +5,9 @@ do
 	./proxy_lib/nntime.sh >> ./proxylist_tmp
 	./proxy_lib/hidemyass.py >> ./proxylist_tmp
 	./proxy_lib/checkerproxy.sh >> ./proxylist_tmp
-	./proxy_lib/usproxy.sh >> ./proxylist_tmp
+	./proxy_lib/freeproxylist.sh >> ./proxylist_tmp
 	./proxy_lib/proxylist.sh >> ./proxylist_tmp
+	#####################################
 	cat ./proxylist* | sort -uR >> ./proxylist
 	rm proxylist_tmp
 	echo "`date '+%Y/%m/%d %H:%M'` Updated"
--- proxy_lib/usproxy.sh
+++
@@ -1,1 +0,0 @@
-w3m -dump -T text/html -cols 120 http://www.us-proxy.org/ | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{printf("%s:%s\n", $1,$2)}'
